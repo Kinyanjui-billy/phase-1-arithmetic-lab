7e97078baf14787a1f369169f7fd48336cebd477
Billy Kinyanjui <billy.kinyanjui@student.moringaschool.com>
2022-08-20 13:48:50 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+    const num1 = 31;
+    const num2 = 2;
+
+   const multiply=(num1 * num2);
+
+   const random= 4
+    Math.floor(Math.random() * 10) + 1;
+   
+    const num3 = 39
+    const num4 = 7
+    const mod =num3%num4
+
+    const max = 20
+    Math.max(11,8,17,16,10,20,9,7);
+   
+
+   
